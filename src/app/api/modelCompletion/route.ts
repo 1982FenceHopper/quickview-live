cc026424746bbcde513dc408197c4ac879be86d8
1982FenceHopper <androxusmusic7001@gmail.com>
2024-11-09 11:31:19 -0500

    Refixed Completion Route #2

--- src/app/api/modelCompletion/route.ts
+++ src/app/api/modelCompletion/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     ],
   });
 
-  return new NextResponse<ReadableStream>(completion.toReadableStream(), {
+  return new Response(completion.toReadableStream(), {
     headers: {
       "Content-Type": "application/json",
       "Transfer-Encoding": "chunked",
